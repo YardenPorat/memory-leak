cd60e96f6d400f9d8420523c11fccbb9edfe2acd
Yarden Porat <yardenporat@gmail.com>
2022-02-23 08:59:25 +0200

    rename board

--- _wcs/boards/MemoryComp/MemoryComp.board.tsx
+++ _wcs/boards/MemoryComp/MemoryComp.board.tsx
@@ -3,6 +3,6 @@ import { createBoard } from '@wixc3/react-board';
 import { MemoryComp } from '../../../src/components/memory-comp';
 
 export default createBoard({
-    name: 'MemoryComp',
-    Board: () => <MemoryComp />
+    name: 'MemoryComp Board',
+    Board: () => <MemoryComp />,
 });
